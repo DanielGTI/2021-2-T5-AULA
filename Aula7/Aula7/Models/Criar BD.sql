cba33a7398846d84cce3b23186ecaa29b965fea5
Daniel <daniel.fb.jr@gmail.com>
2021-11-08 11:34:52 -0300

    Aula 7

---
+++ Aula7/Aula7/Models/Criar BD.sql
@@ -0,0 +1,13 @@
+﻿use[AVA5]
+
+DROP TABLE aluno;
+
+CREATE TABLE aluno(
+	id		int primary key not null identity(1,1),
+	nome	varchar(100),
+	ra		varchar(15),
+	cpf		varchar(15),
+	idade	int
+);
+
+SELECT * FROM aluno;
